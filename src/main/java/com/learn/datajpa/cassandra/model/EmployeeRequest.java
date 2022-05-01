cec1f2d968f945271df9a444d4a4aabf013c71c5
Bed Singh <bedsingh@Beds-MacBook-Pro.local>
2022-05-01 17:34:43 -0500

    Added Project lombok - mapstruct binding.

--- src/main/java/com/learn/datajpa/cassandra/model/EmployeeRequest.java
+++ src/main/java/com/learn/datajpa/cassandra/model/EmployeeRequest.java
@@ -41,49 +41,8 @@ public class EmployeeRequest implements Serializable {
     @Email(message = "Valid email is required.")
     private String email;
 
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
     @NotNull(message="Salary is empty.")
     private Double salary;
-
 
 
     @Override
--- src/main/java/com/learn/datajpa/cassandra/model/EmployeeVO.java
+++ src/main/java/com/learn/datajpa/cassandra/model/EmployeeVO.java
@@ -33,44 +33,4 @@ public class EmployeeVO implements Serializable {
     private String email;
     private Double salary;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
 }
